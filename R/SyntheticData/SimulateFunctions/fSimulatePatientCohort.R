7655cbe126436d6bbd0d04c5031086494f91735a
Dan Wilson <daniel.gb.wilson@gmail.com>
2026-04-27 18:01:22 +1000

    Included two scripts that generate patient age and sex for each patient. The sex demographic is sampled from a Bernoulli distribution with a practice level random effect. The age variable is sampled from a censored normal distribution with PHN level random effect and a fixed effect for sex.

--- R/SyntheticData/SimulateFunctions/fSimulatePatientCohort.R
+++ R/SyntheticData/SimulateFunctions/fSimulatePatientCohort.R
@@ -8,5 +8,13 @@ fSimulatePatientCohort <- function(min_patients_per_prac,
                                                   max_patients_per_prac,
                                                   nb_practices)
   
+  # generate sex of each patient with a practice level random effect
+  
+  patient_cohort <- patient_cohort |> fSimulatePatientSex(nb_practices)
+  
+  # generate age of each patient with a fixed effect for sex and a random effect for PHN
+  
+  patient_cohort <- patient_cohort |> fSimulatePatientAge(nb_practices)
+  
   return(patient_cohort)
 }
